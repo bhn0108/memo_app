00c6a644aea868da625f6cca1e363c8ab65483e2
bhn0108 <bhn0108@icloud.com>
2021-07-04 23:31:36 +0900

    add page transition

--- php/index.php
+++ php/index.php
@@ -21,19 +21,45 @@
     <?php
 
     require('dbconnect.php');
-    
+
+    if (isset($_REQUEST['page']) && is_numeric($_REQUEST['page'])) {
+        $page = $_REQUEST['page'];
+    } else {
+        $page = 1;
+    }
+
+    $start = 5 * ($page - 1);
+
     //一覧取得
-    $query = 'select * from memos order by id desc';
-    $records = $db->query($query);
+    $query = 'select * from memos order by id desc limit ?, 5';
+    $records = $db->prepare($query);
+    $records->bindParam(1, $start, PDO::PARAM_INT);
+    $records->execute();
     ?>
 
     <!-- メモの内容を一行づつ取り出し作成日時とともに一覧表示 -->
     <article>
-    <?php while($record = $records->fetch()): ?>
+        <?php while($record = $records->fetch()): ?>
             <p><a href="memo.php?id=<?php print($record['id']); ?>"><?php print(mb_substr($record['memo'], 0, 50)); ?></a></p>
             <time><?php print($record['created_at']); ?></time>
             <hr>
-    <?php endwhile; ?>
+        <?php endwhile; ?>
+        
+        <!-- ページ遷移リンク -->
+        <!-- 2ページ目以上のとき1ページ前へのリンクを表示 --> 
+        <?php if($page >= 2): ?>
+            <a href="index.php?page=<?php print($page-1); ?>"><?php print($page-1); ?>ページ目へ</a>
+        <?php endif; ?>
+        　|
+        <?php
+            $counts = 'select count(*) as cnt from memos';
+            $counts = $db->query($counts);
+            $count = $counts->fetch();
+            $max_page = ceil($count['cnt'] / 5);
+            if ($page < $max_page):
+        ?>
+            <a href="index.php?page=<?php print($page+1); ?>"><?php print($page+1); ?>ページ目へ</a>
+        <?php endif; ?>
     </article>
 
 </pre>
